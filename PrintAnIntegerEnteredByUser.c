1bd61112c4e5cc0b1751f9a7942f71bea188261f
lpiaitdepok <lpiaitdepok@gmail.com>
2018-10-12 14:53:14 +0700

    Create PrintAnIntegerEnteredByUser.c

---
+++ PrintAnIntegerEnteredByUser.c
@@ -0,0 +1,20 @@
+// Problem Statement: 
+// Write a C program to take an integer number from user and print it on screen.
+
+// Required Knowledge: 
+// C Input/Output, C Variables,  C Datatypes
+
+#include <stdio.h>
+
+int main()
+{
+    int num; // Declaring integer variable 'num' to hold value of number entered by user
+    
+    printf("Enter an integer number:"); // Displaying guide message to user 
+    
+    scanf("%d", &num); // Read integer number using '%d'
+    
+    printf("Entered number is %d", num); // Displaying 'num' on screen
+
+    return 0;
+}
